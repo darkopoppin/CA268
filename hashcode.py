493251033f60a6cdbaa05764c9522b7b69cc009a
Bozhidar Sabahov <33690328+darkopoppin@users.noreply.github.com>
2019-03-08 01:33:37 +0000

    2nd attempt

--- hashcode.py
+++ hashcode.py
@@ -7,9 +7,9 @@ class Slide(object):
         self.tags = tags 
 
     def __str__(self):
-        return 'id='+str(self.id1)+' n_tags='+ str(self.n_tags) +' tags='+ str(self.tags)
+        return 'id='+str(self.id1)+ ' id2='+str(self.id2) + ' n_tags='+ str(self.n_tags) +' tags='+ str(self.tags)
 
-def sort_input(odd_v, even_v, odd_h, even_h,input_pics):
+def sort_input(odd_v, even_v, odd_h, even_h, input_pics):
     for i in range(1, len(input_pics)):
         layout = input_pics[i][0]
         n_tags = int(input_pics[i][1])
@@ -17,22 +17,69 @@ def sort_input(odd_v, even_v, odd_h, even_h,input_pics):
         if layout == 'V':
             if n_tags % 2 == 0: #if its even and vertical
                 even_v.append(Slide(i, n_tags, tags))
-            else: #if its odd and vertical
+            elif n_tags != 1: #if its odd and vertical
                 odd_v.append(Slide(i, n_tags, tags))
         else:
             if n_tags % 2 == 0: #if its even and horizontal
                 even_h.append(Slide(i, n_tags, tags))
-            else: #if its odd and horizontal
+            elif n_tags != 1: #if its odd and horizontal
                 odd_h.append(Slide(i, n_tags, tags))
 
     return odd_v, even_v, odd_h, even_h
 
 def vertical_pairing(odd_v, even_v):
-    pass
+    v_pairs = [] #list for paired vertical pics
+    i = 1
+    while len(odd_v) != 0 and len(even_v) != 0:
+        i = 1
+        ver1 = even_v.pop() #first vertical pic
+        while i < len(even_v): #searches for pair of ver1
+            even_pic = len(ver1.tags.intersection(even_v[-i].tags)) #ver1 intersected with pic from even_v
+            odd_pic = len(ver1.tags.intersection(odd_v[-i].tags)) #ver1 intersected with pic from odd_v
+            if even_pic > odd_pic and even_pic > 1: #compares number of intersected tags 
+                ver2 = even_v.pop(-i)
+                union = ver1.tags.union(ver2.tags)
+                #print(even_pic)
+                v_pairs.append(Slide(ver1.id1, len(union), union, ver2.id1))
+                i = 1
+                break
+            elif odd_pic >= even_pic and odd_pic > 1: #compares number of intersected tags
+                ver2 = odd_v.pop(-i)
+                union = ver1.tags.union(ver2.tags)
+                #print(odd_pic)
+                v_pairs.append(Slide(ver1.id1, len(union), union, ver2.id1))
+                i = 1
+                break
+            i += 1
+    return v_pairs
+
+def make_slideshow(even_h, odd_h):
+    slideshow = []
+    slide = even_h.pop()
+    while len(odd_h) != 2 and len(even_h) != 2:
+        i = 1
+        while i < len(even_h):
+            #print(len(even_h), len(odd_h))
+            even_match = len(slide.tags.intersection(even_h[-i].tags))
+            odd_match = len(slide.tags.intersection(odd_h[-i].tags))
+            if even_match > odd_match and even_match > 0:
+                slideshow.append(even_h.pop(-i))
+                #print('even')
+                break
+            elif odd_match >= even_match and odd_match > 0:
+                slideshow.append(odd_h.pop(-i))
+                #print('odd')
+                break
+            elif abs(odd_match - even_match) <= 1:
+                even_h.pop(-i)
+                odd_h.pop(-i)
+                break
+            i += 1
+    return slideshow
 
 def main():
 
-    with open ('3','r') as f: #read input
+    with open ('1','r') as f: #read input
         line = f.readline()
         input_pics = []
         while len(line) != 0:
@@ -43,6 +90,30 @@ def main():
     odd_v.sort(key=lambda x: x.n_tags, reverse=False)
     even_v.sort(key=lambda x: x.n_tags, reverse=False)
     
-    print(len(odd_v))
-    print(len(even_v))
+    #print('odd',len(odd_v))
+    #print('even', len(even_v))
+    v_pairs = vertical_pairing(odd_v, even_v)
+    print('odd',len(odd_h))
+    print('even', len(even_h))
+    print('v_pairs',len(v_pairs))
+    
+
+    for pair in v_pairs: #distributes the vertical pairs among the horizontal pics
+        if pair.n_tags % 2 == 0:
+            even_h.append(pair)
+        else:
+            odd_h.append(pair)
+    slideshow = make_slideshow(even_h, odd_h)     
+    print('odd',len(odd_h))
+    print('even', len(even_h))
+    print(len(slideshow))
+
+    with open('output','a') as out:
+        out.write(str(len(slideshow)) + '\n')
+        for slide in slideshow:
+            if slide.id2 == None:
+                out.write(str(slide.id1) + '\n')
+            else:
+                out.write(str(slide.id1) + ' ' + str(slide.id2) + '\n')
+
 main()
